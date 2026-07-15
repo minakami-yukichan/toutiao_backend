b1197d413e113e1007c0608fc3e8c41f73a3a37e
minakami-yukichan <2681730489@qq.com>
2026-07-15 14:38:31 +0800

    feat: cors中间件

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routers import news
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源，开发时允许所有，生产环境需要指定源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有HTTP方法
+    allow_headers=["*"],  # 允许所有请求头
+)
 
 @app.get("/")
 async def read_root():
