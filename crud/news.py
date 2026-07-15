07efa7019c735677bd2b0fc05156666ae48df3a0
minakami-yukichan <2681730489@qq.com>
2026-07-15 20:36:25 +0800

    feat: 详情接口

--- crud/news.py
+++ crud/news.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, func
+from sqlalchemy import select, func, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from models.news import Category, News
 
@@ -16,3 +16,32 @@ async def get_news_count(db: AsyncSession, category_id: int):
     stmt = select(func.count(News.id)).where(News.category_id == category_id)
     result = await db.execute(stmt)
     return result.scalar_one()  # scalar_one() 方法返回单个标量值，如果查询结果不止一个或没有结果，会抛出异常
+
+async def get_news_detail(db: AsyncSession, news_id: int):
+    stmt = select(News).where(News.id == news_id)
+    result = await db.execute(stmt)
+    return result.scalar_one_or_none()  # scalar_one_or_none() 方法返回单个标量值，如果查询结果不止一个，会抛出异常，如果没有结果，返回 None
+
+async def increase_news_views(db: AsyncSession, news_id: int):
+    stmt = update(News).where(News.id == news_id).values(views = News.views + 1)
+    result = await db.execute(stmt)
+    await db.commit()  # 提交事务，确保更新生效
+
+    # 命中返回true， 否则返回false
+    return result.rowcount > 0
+
+async def get_related_news(db: AsyncSession, news_id: int, category_id: int, limit: int = 5):
+    stmt = select(News).where(News.category_id == category_id, News.id != news_id).order_by(News.views.desc(), News.publish_time.desc()).limit(limit)
+    result = await db.execute(stmt)
+    # return result.scalars().all()
+    related_news = result.scalars().all()
+    # 列表推导式，推导出新闻核心数据再return
+    return [{"id": news_detail.id,
+            "title": news_detail.title,
+            "content": news_detail.content,
+            "image": news_detail.image,
+            "author": news_detail.author,
+            "pushTime": news_detail.publish_time,
+            "categoryId": news_detail.category_id,
+            "views": news_detail.views
+        } for news_detail in related_news]
--- routers/news.py
+++ routers/news.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from config.db_conf import get_db
-from crud.news import get_category, get_news_list as crud_get_news_list, get_news_count as crud_get_news_count
+from crud.news import get_category, get_news_list as crud_get_news_list, get_news_count as crud_get_news_count, get_news_detail as crud_get_news_detail, increase_news_views as crud_increase_news_views, get_related_news as crud_get_related_news
 
 # 创建apirouter实例
 # prefix 路由前缀， tags 路由标签
@@ -48,5 +48,39 @@ async def get_news_list(category_id: int = Query(..., alias="categoryId"),
             "list": news_list,
             "total": total,
             "has_more": has_more
+        }
+    }
+
+@router.get("/detail")
+async def get_news_detail(news_id: int = Query(..., alias="id"), db: AsyncSession = Depends(get_db)):
+    """
+    获取新闻详情
+    """
+    # 先获取数据库中的新闻数据 -> 定义模型类 -> 封装获取数据方法
+    news_detail = await crud_get_news_detail(db, news_id=news_id)
+    if not news_detail:
+        raise HTTPException(status_code=404, detail="新闻不存在")
+    
+    # 增加新闻浏览量
+    views_result = await crud_increase_news_views(db, news_id=news_id)
+    if not views_result:
+        raise HTTPException(status_code=500, detail="增加新闻浏览量失败")
+    
+    # 获取相关新闻列表
+    related_news = await crud_get_related_news(db, news_id=news_id, category_id=news_detail.category_id)
+
+    return {
+        "code": 200,
+        "message": "获取新闻详情成功",
+        "data": {
+            "id": news_detail.id,
+            "title": news_detail.title,
+            "content": news_detail.content,
+            "image": news_detail.image,
+            "author": news_detail.author,
+            "pushTime": news_detail.publish_time,
+            "categoryId": news_detail.category_id,
+            "views": news_detail.views,
+            "relatedNews": related_news
         }
     }
